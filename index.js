629576a44802b326efb932ef310007274fd54699
Dee Race <deerace12@gmail.com>
2018-07-26 19:36:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution in this file!
+
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+  customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  var bestCustomer = 'not bob'
+}
